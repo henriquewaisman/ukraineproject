113b3788a43ffebbf0bd397cac22d0fbc7c1f45a
Henrique Waisman <100635335+henriquewaisman@users.noreply.github.com>
2022-05-25 13:39:51 -0300

    Update wanto.js

--- foreignjs/wanto.js
+++ foreignjs/wanto.js
@@ -1,29 +1,35 @@
-init()
-
-function init(){
- document.getElementById("nome").addEventListener("keyup", salvar)
- document.getElementById("age").addEventListener("keyup", salvar)
- document.getElementById("lang").addEventListener("keyup", salvar)
- document.getElementById("time").addEventListener("keyup", salvar)
- document.getElementById("son").addEventListener("keyup", salvar)
- document.getElementById("sex").addEventListener("keyup", salvar)
- document.getElementById("sonage").addEventListener("keyup", salvar)
-}
-
 function salvar(){
   var nome = document.getElementById("nome").value
   var age = document.getElementById("age").value
   var lang = document.getElementById("lang").value
   var time = document.getElementById("time").value
-  var son = document.getElementById("son").value
-  var sex = document.getElementById("sex").value
-  var sonage = document.getElementById("sonage").value
+  var cpf = document.getElementById("cpf").value
+  var adress = document.getElementById("adress").value
+  var morador = document.getElementById("morador").value
+  var about = document.getElementById("about").value
+  var personaimg = document.getElementById("personaimg").value
 
   localStorage.setItem("user", nome)
   localStorage.setItem("firstage", age)
   localStorage.setItem("languages", lang)
   localStorage.setItem("timestaying", time)
-  localStorage.setItem("son", son)
-  localStorage.setItem("sex", sex)
-  localStorage.setItem("secondage", sonage)
+  localStorage.setItem("cpf", cpf)
+  localStorage.setItem("adress", adress)
+  localStorage.setItem("morador", morador)
+  localStorage.setItem("aboutext", about)
+  localStorage.setItem("perfil", personaimg)
+
+  location.replace("/last/lastscreen.html")
+}
+
+function redefinir(){
+  document.getElementById('nome').value = ''
+  document.getElementById('age').value = ''
+  document.getElementById('lang').value = ''
+  document.getElementById('time').value = ''
+  document.getElementById('cpf').value = ''
+  document.getElementById('adress').value = ''
+  document.getElementById('morador').value = ''
+  document.getElementById('about').value = ''
+  document.getElementById('personaimg').src = '/perfil.jpg'
 }
